e0ebe2ac113f1e7ab57e744c4de25ce1c3264560
Julian Chingal <70417427+Julian-Chingal@users.noreply.github.com>
2026-06-23 16:38:20 -0500

    feat: replace ApiOkResponse with ApiEntityResponse for health check endpoint

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,9 +1,10 @@
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 import { PrismaHealthIndicator } from './prisma.health';
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { RedisHealthIndicator } from './redis.health';
 import { HealthResponse } from './dto/responses.dto';
+import { ApiEntityResponse } from '@shared/swagger/decorators';
 
 @ApiTags('Health')
 @Controller('health')
@@ -19,10 +20,7 @@ export class HealthController {
   @ApiOperation({
     summary: 'Verificar el estado de salud de la aplicación y sus dependencias',
   })
-  @ApiOkResponse({
-    description: 'Todos los servicios están operativos.',
-    type: HealthResponse,
-  })
+  @ApiEntityResponse(HealthResponse, 'Todos los servicios están operativos.')
   check() {
     return this.health.check([
       () => this.prismaHealth.isHealthy('database'),
